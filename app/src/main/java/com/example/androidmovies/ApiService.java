6f5094fe25ab5310de78252270f1b83fed3c6d57
Норберт Ягмуров <nuri.handsome45@gmail.com>
2026-09-04 22:48:32 +0300

    created 3 drawable files that contain circle shape for bad, secondary and good films with 3 background colors. In adapter files getting movie rating and based on that rating creating proper background shape for rating textView. Changed string type of rating to double. Tested in query with various types of films and convinced that every rating shows with correct background.

--- app/src/main/java/com/example/androidmovies/ApiService.java
+++ app/src/main/java/com/example/androidmovies/ApiService.java
@@ -5,6 +5,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiService {
-    @GET("v1.4/movie?token=FNE5FN8-B0KM23T-KBAA52K-G7G30Q8&rating.kp=7-10&sortField=votes.kp&sortType=-1&limit=5")
+    @GET("v1.4/movie?token=FNE5FN8-B0KM23T-KBAA52K-G7G30Q8&rating.kp=7-10&sortField=votes.kp&sortType=-1&limit=10")
     Single<ServerResponce> getMovies(@Query("page") int page);
 }
--- app/src/main/java/com/example/androidmovies/MoviesAdapter.java
+++ app/src/main/java/com/example/androidmovies/MoviesAdapter.java
@@ -1,6 +1,7 @@
 package com.example.androidmovies;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -43,7 +45,18 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         Glide.with(holder.itemView)
                 .load(movie.getPoster().getUrl())
                 .into(holder.imageViewPoster);
-        holder.textViewRating.setText(movie.getRating().getKp().substring(0, 3));
+        double rating = movie.getRating().getKp();
+        int backgroundId;
+        if (rating > 7){
+            backgroundId = R.drawable.circle_green;
+        } else if (rating > 5){
+            backgroundId = R.drawable.circle_yellow;
+        } else {
+            backgroundId = R.drawable.circle_red;
+        }
+        Drawable background = ContextCompat.getDrawable(holder.itemView.getContext(), backgroundId);
+        holder.textViewRating.setBackground(background);
+        holder.textViewRating.setText(String.valueOf(rating).substring(0, 3));
     }
 
     @Override
